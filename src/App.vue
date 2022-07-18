42e55723f493224278e8408fc50e9f8c1e2c540c
charlenetormes <charlene@inspireteam.com>
2022-07-18 14:32:51 +0800

    Configure prettier

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <h1>Hello World!</h1>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+export default {
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
