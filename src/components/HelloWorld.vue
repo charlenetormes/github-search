51a215b82fd42a902ffd0e77e86a988f29c9d490
charlenetormes <charlene@inspireteam.com>
2022-07-23 22:25:49 +0800

    Vue3 Setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,14 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
+
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+
   <h1>{{ msg }}</h1>
 
   <div class="card">
@@ -31,8 +33,37 @@ const count = ref(0)
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
+<script lang="ts">
+
+import { ref } from "vue";
+
+export default {
+  name: "HelloWorld",
+  props: ["msg"],
+  setup() {
+    const count = ref(0);
+
+    return {
+      count
+    }
+  },
+}
+
+</script>
+
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
