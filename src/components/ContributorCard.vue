7949b04233064dd9d844acd628a0acfa3db14453
charlenetormes <charlene@inspireteam.com>
2022-07-24 18:27:13 +0800

    Get all contributors of the repository

--- src/components/ContributorCard.vue
+++ src/components/ContributorCard.vue
@@ -1,16 +1,23 @@
 <template>
-    <div class="flex justify-center items-center mt-12">
-        Hello
-        <!-- <div class="block p-6 rounded-lg shadow-lg bg-white w-3/6">
+    <div v-if="repo" class="flex justify-center items-center mt-12">
+        <div class="block p-6 rounded-lg shadow-lg bg-white w-3/6">
             <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">{{repo.full_name}}</h5>
             <p class="text-gray-700 text-base mb-4">
                 Owner: {{repo.owner.login}}
             </p>
-            <h3 class="text-gray-900 text-md leading-tight font-medium mb-2">Contributors</h3>
-            <ul>
-                <li></li>
-            </ul>
-        </div> -->
+            <div v-if="contributors.length > 0">
+                <h3 class="text-gray-900 text-md leading-tight font-medium mb-2">Contributors</h3>
+                <ul>
+                    <li v-for="(item,index) in contributors" :key="index">
+                        <a :href="item.html_url">
+                            <p class="text-gray-900 text-md leading-tight font-medium mb-2">{{item.login}}</p>
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+            
+        </div>
     </div>    
 </template>
 
@@ -24,10 +31,15 @@ export default {
     data() {
         return {
             repo: {},
-            contributors: [{}]
+            contributors: [{}],
+            url: ''
         }
     },
     methods: {
+        async init(){
+            this.getRepo();
+            // await this.getContributors();
+        },
         async getRepo(){
             this.repo = await axios.get(`https://api.github.com/repositories/${this.repoId}`,{
                 headers: {
@@ -40,10 +52,33 @@ export default {
             .catch((e) => {
                 return null;
             });
+            
+            this.repo;
+        },
+
+        async getContributors(){
+            this.contributors = await axios.get(`${this.url}`, {
+                headers: {
+                    'Authorization': `token ${token}`
+                }
+            })
+            .then((res) => {
+                const { data } = res;
+                console.log(data);
+                return data;
+            })
+            .catch((e) => {
+                
+                return null;
+            })
         }
     },
-    onBeforeMount(){
-        this.getRepo();
+    beforeMount(){
+        this.init();
+    },
+    mounted(){
+        this.url = this.repo.contributors_url;
+        this.getContributors();
     }
 }
 </script>
